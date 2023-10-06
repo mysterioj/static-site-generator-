2b2b29660c04457831eb9f0f915e8d8660521b24
egor <8541934@mail.ru>
2023-10-06 15:59:11 +0300

    add multiple language support

--- index.js
+++ index.js
@@ -32,17 +32,55 @@ try {
 } catch(err) {}
 
 try {
-	getMarkdown(join(path, 'texts/ru/'))
+	getTemplates(join(path, 'partials/'))
+		.forEach((file) => {
+			var data = fs.readFileSync(file, 'utf8');
+			var name = basename(file, '.hbs');
+			Handlebars.registerPartial(name, data);
+		});
+
+	let langs = parseLangs(join(path, 'texts/'));
+	console.log("Detected languages: " + langs);
+
+	langs.forEach((lang) => {
+		registerTexts(join(path, join('texts', lang)));
+
+		getTemplates(join(path, 'pages/'))
+			.forEach((file) => {
+				var data = fs.readFileSync(file, 'utf8');
+				const template = Handlebars.compile(data);
+				var data = "{}";
+				const compiled = template(data);
+				if (options.output == undefined) {
+					console.log(compiled);
+				} else {
+					var p = join(join(options.output, lang), file
+						.replace(join(path, 'pages'), '')
+						.replace(extname(file), '.html'));
+					try {
+						fs.mkdirSync(dirname(p), { recursive: true });
+					} catch(err) {}
+					fs.writeFileSync(p, compiled);
+				}
+			});
+	});
+} catch (err) {
+    console.error(err);
+    return;
+}
+
+function registerTexts(path) {
+	getMarkdown(path)
 		.map((file) => {
 			var data = fs.readFileSync(file, 'utf8');
 			return ({ file, data: marked.parse(data) });
 		})
-		.concat(getHtml(join(path, 'texts/ru/'))
+		.concat(getHtml(path)
 			.map((file) => ( { file, data: fs.readFileSync(file, 'utf8') }))
 		)
 		.map(({ file, data }) => {
 			return { 
-				file: file.replace(join(path, 'texts/ru'), '').replace(extname(file), ''),
+				file: file.replace(path, '').replace(extname(file), ''),
 				data
 			}
 		})
@@ -50,34 +88,6 @@ try {
 			const partialName = join('texts', file).replaceAll('/', '.');
 			Handlebars.registerPartial(partialName, data);
 		});
-
-	getTemplates(join(path, 'partials/'))
-		.map((file) => {
-			var data = fs.readFileSync(file, 'utf8');
-			var name = basename(file, '.hbs');
-			Handlebars.registerPartial(name, data);
-		});
-	getTemplates(join(path, 'pages/'))
-		.forEach((file) => {
-			var data = fs.readFileSync(file, 'utf8');
-			const template = Handlebars.compile(data);
-			var data = "{}";
-			const compiled = template(data);
-			if (options.output == undefined) {
-				console.log(compiled);
-			} else {
-				var p = options.output+file
-					.replace(join(path,'pages'), '')
-					.replace(extname(file), '.html');
-				try {
-					fs.mkdirSync(dirname(p), { recursive: true });
-				} catch(err) {}
-				fs.writeFileSync(p, compiled);
-			}
-		});
-} catch (err) {
-    console.error(err);
-    return;
 }
 
 function getMarkdown(path) {
@@ -108,4 +118,11 @@ function getFilesRecursive(path) {
 		}
 	});
 	return result;
+}
+
+function parseLangs(path) {
+	var files = fs.readdirSync(path);
+	return files
+		.filter((file) => fs.lstatSync(join(path, file)).isDirectory())
+		.map((dir) => dir)
 }
